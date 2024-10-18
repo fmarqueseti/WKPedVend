8641ae07201c978e6f20b60ec041a3d0ab8c976f
Fábio Marques <fmarques@fmarques.eti.br>
2024-10-18 18:39:20 -0300

    hotfix 01
    - Adoção do padrão DAO.
    - Aumento na capacidade de armazenamento dos campos do tipo DECIMAL no SGBD.

--- PedidoVenda.dpr
+++ PedidoVenda.dpr
@@ -4,11 +4,16 @@ uses
   Vcl.Forms,
   untPrincipal in 'View\untPrincipal.pas' {frmPrincipal},
   untConexao in 'Banco\untConexao.pas',
-  untFachadaBanco in 'Banco\untFachadaBanco.pas',
   untCliente in 'Model\untCliente.pas',
   untProduto in 'Model\untProduto.pas',
   untPedidoCabecalho in 'Model\untPedidoCabecalho.pas',
-  untPedidoDetalhe in 'Model\untPedidoDetalhe.pas';
+  untPedidoDetalhe in 'Model\untPedidoDetalhe.pas',
+  untIClienteDAO in 'DAO\untIClienteDAO.pas',
+  untIPedidoCabDAO in 'DAO\untIPedidoCabDAO.pas',
+  untIProdutoDAO in 'DAO\untIProdutoDAO.pas',
+  untClienteDAO in 'DAO\Impl\untClienteDAO.pas',
+  untPedidoCabDAO in 'DAO\Impl\untPedidoCabDAO.pas',
+  untProdutoDAO in 'DAO\Impl\untProdutoDAO.pas';
 
 {$R *.res}
 
